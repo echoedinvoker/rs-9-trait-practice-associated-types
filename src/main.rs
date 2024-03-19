0b7a0afd9b08889f1c57e56a7223bd71c0ea5151
Matt Chang <echoedinvoker@gmail.com>
2024-03-19 17:56:09 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+// Problem 1: Add the correct types for the associated type 'item' in the implemenation blocks
+trait Container {
+    type Item;
+
+    fn add_item(&mut self, item: Self::Item);
+    fn get_item(&self) -> Option<&Self::Item>;
+}
+
+struct VecContainer_i32 {
+    items: Vec<i32>,
+}
+
+impl Container for VecContainer_i32 {
+    type Item = ?; // This line needs a fix
+
+    fn add_item(&mut self, item: Self::Item) {
+        self.items.push(item);
+    }
+
+    fn get_item(&self) -> Option<&i32> {
+        self.items.last()
+    }
+}
+
+struct OptionContainer<T> {
+    item: Option<T>,
+}
+
+impl<T> Container for OptionContainer<T> {
+    type Item = ?; // This line needs a fix
+
+    fn add_item(&mut self, item: T) {
+        self.item = Some(item);
+    }
+
+    fn get_item(&self) -> Option<&T> {
+        self.item.as_ref()
+    }
+}
+
+fn main() {
+    let mut vec_container = VecContainer_i32 { items: Vec::new() };
+    vec_container.add_item(42);
+    vec_container.add_item(123);
+
+    if let Some(last_item) = vec_container.get_item() {
+        println!("Last item in VecContainer: {}", last_item);
+    } else {
+        println!("VecContainer is empty");
+    }
+
+    let mut option_container = OptionContainer { item: None };
+    option_container.add_item("Hello, Rust!");
+
+    if let Some(only_item) = option_container.get_item() {
+        println!("Only item in OptionContainer: {}", only_item);
+    } else {
+        println!("OptionContainer is empty");
+    }
+}
+
